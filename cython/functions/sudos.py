66babc45de3afb959ef516b909cecc685c423621
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-04-08 12:54:56 +0430

    Update sudos.py

--- cython/functions/sudos.py
+++ cython/functions/sudos.py
@@ -49,7 +49,7 @@ def add_sudo(id):  # Take int or str with numbers only , Returns Boolean
         udB.set("SUDOS", list_to_str(sudos))
         return True
     except Exception as e:
-        print(f"CɪᴘʜᴇʀX Bot LOG : // functions/sudos/add_sudo : {e}")
+        print(f"CɪᴘʜᴇʀX ᴇxᴄlusivᴇ ʙᴏᴛ LOG : // functions/sudos/add_sudo : {e}")
         return False
 
 
@@ -63,5 +63,5 @@ def del_sudo(id):  # Take int or str with numbers only , Returns Boolean
         udB.set("SUDOS", list_to_str(sudos))
         return True
     except Exception as e:
-        print(f"CɪᴘʜᴇʀX Bot LOG : // functions/sudos/del_sudo : {e}")
+        print(f"CɪᴘʜᴇʀX ᴇxᴄlusivᴇ ʙᴏᴛ LOG : // functions/sudos/del_sudo : {e}")
         return False
