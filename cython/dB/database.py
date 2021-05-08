164c702af59cc9560c63036b206e5e96a725ba8e
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-05-08 09:52:18 +0430

    Update database.py

--- cython/dB/database.py
+++ cython/dB/database.py
@@ -6,20 +6,16 @@ load_dotenv(find_dotenv())
 
 class Var(object):
     # Mandatory
-    API_ID = config("API_ID", default=None, cast=int)
+    API_ID = config("API_ID", default=0, cast=int)
     API_HASH = config("API_HASH", default=None)
     BOT_TOKEN = config("BOT_TOKEN", default=None)
-    BOT_USERNAME = config("BOT_USERNAME", default=None)
     SESSION = config("SESSION", default=None)
     DB_URI = config("DATABASE_URL", default=None)
     LOG_CHANNEL = config("LOG_CHANNEL", default=None, cast=int)
     BLACKLIST_CHAT = set(int(x) for x in config("BLACKLIST_CHAT", "").split())
     # bot mode
     BOT_MODE = config("BOT_MODE", default=False, cast=bool)
-    try:
-        OWNER_ID = config("OWNER_ID", default=None, cast=int)
-    except BaseException:
-        pass
+    OWNER_ID = config("OWNER_ID", default=0, cast=int)
     # heroku stuff
     try:
         HEROKU_APP_NAME = config("HEROKU_APP_NAME", default=None)
