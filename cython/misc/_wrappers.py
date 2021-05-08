943d3533db097f450d6aed93755986dfecbb1a83
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-05-08 10:42:55 +0430

    Update _wrappers.py

--- cython/misc/_wrappers.py
+++ cython/misc/_wrappers.py
@@ -9,10 +9,10 @@ from ..functions.sudos import *
 
 async def eod(event, text=None, **args):
     args["func"] = lambda e: e.via_bot_id is None
-    time = args.get("time", None)
+    time = args.get("time", 5)
     link_preview = args.get("link_preview", False)
     parse_mode = args.get("parse_mode", "md")
-    if is_sudo(event.sender_id):
+    if is_sudo(event.sender_id) or (BOT_MODE and event.sender_id == ultroid_bot.uid):
         replied = await event.get_reply_message()
         if replied:
             ult = await replied.reply(
@@ -24,11 +24,10 @@ async def eod(event, text=None, **args):
             )
     else:
         ult = await event.edit(text, link_preview=link_preview, parse_mode=parse_mode)
-    if time is None:
-        return ult
-    else:
+    if time:
         await asyncio.sleep(time)
         return await ult.delete()
+    return ult
 
 
 # sudo
@@ -51,10 +50,23 @@ def sudo():
 # edit or reply
 
 
-async def eor(event, text):
-    if is_sudo(event.sender_id):
+async def eor(event, text, **omk):
+    link_preview = omk.get("link_preview", False)
+    parse_mode = omk.get("parse_mode", "md")
+    time = omk.get("time", None)
+    if is_sudo(event.sender_id) or (BOT_MODE and event.sender_id == ultroid_bot.uid):
         reply_to = await event.get_reply_message()
         if reply_to:
-            return await reply_to.reply(text)
-        return await event.reply(text)
-    return await event.edit(text)
+            ok = await reply_to.reply(
+                text, link_preview=link_preview, parse_mode=parse_mode
+            )
+        else:
+            ok = await event.reply(
+                text, link_preview=link_preview, parse_mode=parse_mode
+            )
+    else:
+        ok = await event.edit(text, link_preview=link_preview, parse_mode=parse_mode)
+    if time:
+        await asyncio.sleep(time)
+        return await ok.delete()
+    return ok
