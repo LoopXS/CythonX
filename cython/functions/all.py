cde8ddd4aae4f01a4e4031fe1f1b9b4c682fcdce
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-05-09 14:18:43 +0430

    Update all.py

--- cython/functions/all.py
+++ cython/functions/all.py
@@ -543,7 +543,7 @@ async def upload_file(http, file_path, file_name, mime_type, event, parent_id):
     media_body = MediaFileUpload(file_path, mimetype=mime_type, resumable=True)
     body = {
         "title": file_name,
-        "description": "Uploaded using CɪᴘʜᴇʀX ᴇxᴄlusivᴇ ʙᴏᴛ",
+        "description": "Uploaded using CɪᴘʜᴇʀX ᴇxᴄlusivᴇ ʙᴏᴛ...",
         "mimeType": mime_type,
     }
     if parent_id is not None:
@@ -934,7 +934,7 @@ async def get_chatinfo(event):
             return None
         except ChannelPrivateError:
             await eor(
-                event, "`This is a private channel/group or I am banned from there`"
+                event, "`This is a private channel/group or I'm banned from there`"
             )
             return None
         except ChannelPublicGroupNaError:
