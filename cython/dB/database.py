c41a90756c75cceed27e096eb34733355d668655
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-05-23 17:17:20 +0430

    Update database.py

--- cython/dB/database.py
+++ cython/dB/database.py
@@ -6,12 +6,12 @@ load_dotenv(find_dotenv())
 
 class Var(object):
     # Mandatory
-    API_ID = config("API_ID", default=0, cast=int)
-    API_HASH = config("API_HASH", default=None)
+    API_ID = config("API_ID", default=6, cast=int)
+    API_HASH = config("API_HASH", default="eb06d4abfb49dc3eeb1aeb98ae0f581e")
     BOT_TOKEN = config("BOT_TOKEN", default=None)
     SESSION = config("SESSION", default=None)
     DB_URI = config("DATABASE_URL", default=None)
-    LOG_CHANNEL = config("LOG_CHANNEL", default=None, cast=int)
+    LOG_CHANNEL = config("LOG_CHANNEL", default=0, cast=int)
     BLACKLIST_CHAT = set(int(x) for x in config("BLACKLIST_CHAT", "").split())
     # bot mode
     BOT_MODE = config("BOT_MODE", default=False, cast=bool)
