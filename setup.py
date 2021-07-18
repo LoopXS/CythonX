89950b4ef5425ef5418e3e1877530a60f799269a
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-07-18 20:42:55 +0430

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,6 +15,25 @@ description = "A Secure and Powerful Python-Telethon Based Library For Cɪᴘʜ�
 license = "GNU AFFERO GENERAL PUBLIC LICENSE (v3)"
 url = "https://github.com/CipherX1-ops/cython"
 
+classifiers = [
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "Operating System :: OS Independent",
+]
+requirements = [
+    "redis",
+    "python-decouple==3.3",
+    "py-tgcalls==0.5.2",
+    "python-dotenv==0.15.0",
+    "cloudscraper",
+    "aiofiles",
+    "aiohttp",
+]
+
 setuptools.setup(
     name=name,
     version=version,
@@ -25,18 +44,7 @@ setuptools.setup(
     url=url,
     license=license,
     packages=setuptools.find_packages(),
-    install_requires=[
-        "telethon",
-        "redis",
-        "python-decouple==3.3",
-        "TgCrypto",
-        "python-dotenv==0.15.0",
-        "cloudscraper",
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
-    ],
+    install_requires=requirements,
+    classifiers=classifiers,
     python_requires=">=3.6",
 )
