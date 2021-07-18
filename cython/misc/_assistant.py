b2442d51424115ccca2743f5806bcc96cc5427c9
CipherX1-ops <66735676+CipherX1-ops@users.noreply.github.com>
2021-07-18 20:49:53 +0430

    Update _assistant.py

--- cython/misc/_assistant.py
+++ cython/misc/_assistant.py
@@ -1,17 +1,18 @@
 import functools
 
-from telethon import *
-from telethon import events
-from telethon.tl.types import InputWebDocument
+from telethon.errors.rpcerrorlist import UserNotParticipantError
+from telethon.events import CallbackQuery, InlineQuery, NewMessage
+from telethon.tl.types import (
+    ChannelParticipantAdmin,
+    ChannelParticipantCreator,
+    InputWebDocument,
+    PeerChannel,
+)
 from telethon.utils import get_display_name
 
-from .. import *
-from ..dB.core import *
-from ..utils import *
-from ._decorators import sed
+from .. import asst, ultroid_bot
+from . import owner_and_sudos
 
-OWNER_NAME = ultroid_bot.me.first_name
-OWNER_ID = ultroid_bot.me.id
 ULTROID_PIC = "https://telegra.ph/file/167a0b85048b04129bd3b.jpg"
 MSG = f"""
 **⚜️ CɪᴘʜᴇʀX Ⲉⲭⲥⳑυⲋⲓⳳⲉ ⲃⲟⲧ ⚜️**
@@ -19,6 +20,7 @@ MSG = f"""
 **Owner**: CɪᴘʜᴇʀX
 **✨ CɪᴘʜᴇʀX is the best ✨**
 ✵✵✵✵✵✵✵✵✵✵✵✵✵✵
+"""➖➖➖➖➖➖➖
 """
 
 # decorator for assistant
@@ -71,34 +73,28 @@ def inline_owner():
 def asst_cmd(dec):
     def ult(func):
         pattern = "^/" + dec  # todo - handlers for assistant?
-        ultroid_bot.asst.add_event_handler(
-            func, events.NewMessage(incoming=True, pattern=pattern)
-        )
+        asst.add_event_handler(func, NewMessage(incoming=True, pattern=pattern))
 
     return ult
 
 
-def callback(sed):
+def callback(dat):
     def ultr(func):
-        data = sed
-        ultroid_bot.asst.add_event_handler(
-            func, events.callbackquery.CallbackQuery(data=data)
-        )
+        asst.add_event_handler(func, CallbackQuery(data=dat))
 
     return ultr
 
 
 def inline():
     def ultr(func):
-        ultroid_bot.asst.add_event_handler(func, events.InlineQuery)
+        asst.add_event_handler(func, InlineQuery)
 
     return ultr
 
 
 def in_pattern(pat):
     def don(func):
-        pattern = pat
-        ultroid_bot.asst.add_event_handler(func, events.InlineQuery(pattern=pattern))
+        asst.add_event_handler(func, InlineQuery(pattern=pat))
 
     return don
 
@@ -108,14 +104,36 @@ def owner():
     def decorator(function):
         @functools.wraps(function)
         async def wrapper(event):
-            if event.sender_id in sed:
+            if str(event.sender_id) in owner_and_sudos():
                 await function(event)
             else:
                 try:
-                    await event.answer(f"This is {OWNER_NAME} ᴇxᴄlusivᴇ ʙᴏᴛ")
+                    await event.answer(
+                        f"This is {get_display_name(ultroid.me)} ᴇxᴄlusivᴇ ʙᴏᴛ"
+                    )
                 except BaseException:
                     pass
 
         return wrapper
 
     return decorator
+
+
+async def admin_check(event):
+    # Anonymous Admin Support
+    if not event.sender_id and (
+        isinstance(event.peer_id, PeerChannel)
+        and str(event.peer_id.channel_id) in str(event.chat_id)
+    ):
+        return True
+    if str(event.sender_id) in owner_and_sudos():
+        return True
+    try:
+        perms = await event.client.get_permissions(event.chat_id, event.sender_id)
+    except UserNotParticipantError:
+        return False
+    if isinstance(
+        perms.participant, (ChannelParticipantAdmin, ChannelParticipantCreator)
+    ):
+        return True
+    return False
