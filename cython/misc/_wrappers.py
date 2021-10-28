9865e4252cc514309d54b9c798c19505152457c9
CoderX <66735676+ToxygenX@users.noreply.github.com>
2021-10-28 14:30:04 +0330

    Update _wrappers.py

--- cython/misc/_wrappers.py
+++ cython/misc/_wrappers.py
@@ -1,71 +1,39 @@
 import asyncio
-import functools
-
-from . import sudoers
-
-# edit/reply & delete
-
-
-async def eod(event, text=None, **args):
-    args["func"] = lambda e: e.via_bot_id is None
-    time = args.get("time", 5)
-    link_preview = args.get("link_preview", False)
-    parse_mode = args.get("parse_mode", "md")
-    if not event.out:
-        replied = await event.get_reply_message()
-        if replied:
-            ult = await replied.reply(
-                text, link_preview=link_preview, parse_mode=parse_mode
-            )
-        else:
-            ult = await event.reply(
-                text, link_preview=link_preview, parse_mode=parse_mode
-            )
-    else:
-        ult = await event.edit(text, link_preview=link_preview, parse_mode=parse_mode)
-    if time:
-        await asyncio.sleep(time)
-        return await ult.delete()
-    return ult
-
-
-# sudo
-
-
-def sudo():
-    def decorator(function):
-        @functools.wraps(function)
-        async def wrapper(event):
-            if event.out or str(event.sender_id) in sudoers():
-                await function(event)
-            else:
-                pass
-
-        return wrapper
-
-    return decorator
-
 
 # edit or reply
 
+DEL_TIME = []
 
-async def eor(event, text, **omk):
-    link_preview = omk.get("link_preview", False)
-    parse_mode = omk.get("parse_mode", "md")
-    time = omk.get("time", None)
+
+async def eor(event, text, **args):
+    link_preview = args.get("link_preview", False)
+    parse_mode = args.get("parse_mode", "md")
+    time = args.get("time", None)
     if not event.out:
-        reply_to = await event.get_reply_message()
-        if reply_to:
-            ok = await reply_to.reply(
-                text, link_preview=link_preview, parse_mode=parse_mode
-            )
-        else:
-            ok = await event.reply(
-                text, link_preview=link_preview, parse_mode=parse_mode
-            )
+        reply_to = event.reply_to_msg_id or event
+        ok = await event.client.send_message(
+            event.chat_id,
+            text,
+            link_preview=link_preview,
+            parse_mode=parse_mode,
+            reply_to=reply_to,
+        )
     else:
         ok = await event.edit(text, link_preview=link_preview, parse_mode=parse_mode)
-    if time:
-        await asyncio.sleep(time)
+    if not DEL_TIME:
+        from .. import udB
+
+        ut = udB.get("DEL_DELAY_TIME")
+        DEL_TIME.append(ut)
+    else:
+        ut = DEL_TIME[0]
+    if time and ut != "None":
+        time = ut or time
+        await asyncio.sleep(int(time))
         return await ok.delete()
     return ok
+
+
+async def eod(event, text, **kwargs):
+    kwargs["time"] = kwargs.get("time", 5)
+    return await eor(event, text, **kwargs)
