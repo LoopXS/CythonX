4cc91ca304b733607f5535a3f4fe5cacd90a81fd
CoderX <66735676+ToxygenX@users.noreply.github.com>
2021-10-28 14:16:53 +0330

    Update _assistant.py

--- cython/misc/_assistant.py
+++ cython/misc/_assistant.py
@@ -1,19 +1,17 @@
-import functools
+import inspect
+import re
 
-from telethon.errors.rpcerrorlist import UserNotParticipantError
+from telethon import Button
 from telethon.events import CallbackQuery, InlineQuery, NewMessage
-from telethon.tl.types import (
-    ChannelParticipantAdmin,
-    ChannelParticipantCreator,
-    InputWebDocument,
-    PeerChannel,
-)
+from telethon.tl.types import InputWebDocument
 from telethon.utils import get_display_name
 
-from .. import asst, ultroid_bot
-from . import owner_and_sudos
+from .. import LOGS, asst, ultroid_bot
+from . import append_or_update, owner_and_sudos
 
 ULTROID_PIC = "https://telegra.ph/file/167a0b85048b04129bd3b.jpg"
+OWNER = get_display_name(ultroid_bot.me)
+
 MSG = f"""
 **⚜️ CɪᴘʜᴇʀX Ⲉⲭⲥⳑυⲋⲓⳳⲉ ⲃⲟⲧ ⚜️**
 ✵✵✵✵✵✵✵✵✵✵✵✵✵✵
@@ -22,118 +20,80 @@ MSG = f"""
 ✵✵✵✵✵✵✵✵✵✵✵✵✵✵
 ➖➖➖➖➖➖➖
 """
+IN_BTTS=[
+    [
+        Button.url(
+            "✵CɪᴘʜᴇʀX Ⲃⲟⲧ✵",
+            url="https://t.me/CipherXBot",
+        ),
+        Button.url(
+            "✵Suᴩᴩᴏrᴛ Chᴀnnᴇl✵", 
+            url="https://t.me/FutureTechnologyOfficial"
+        ),
+    ]
+]
+
 
 # decorator for assistant
 
 
-def inline_owner():
-    def decorator(function):
-        @functools.wraps(function)
+def asst_cmd(pattern=None, load=None, **kwargs):
+    """Decorator for assistant's command"""
+    name = inspect.stack()[1].filename.split("/")[-1].replace(".py", "")
+
+    def ult(func):
+        if pattern:
+            kwargs["pattern"] = re.compile("^/" + pattern)
+        asst.add_event_handler(func, NewMessage(**kwargs))
+        if load is not None:
+            append_or_update(load, func, name, kwargs)
+
+    return ult
+
+
+def callback(data=None, owner=False, **kwargs):
+    """Assistant's callback decorator"""
+
+    def ultr(func):
         async def wrapper(event):
-            if str(event.sender_id) in owner_and_sudos():
-                try:
-                    await function(event)
-                except BaseException:
-                    pass
-            else:
-                try:
-                    builder = event.builder
-                    sur = builder.article(
+            if owner and not str(event.sender_id) in owner_and_sudos():
+                return await event.answer(f"This is {OWNER} ᴇxᴄlusivᴇ ʙᴏᴛ")
+            try:
+                await func(event)
+            except Exception as er:
+                LOGS.exception(er)
+
+        asst.add_event_handler(wrapper, CallbackQuery(data=data, **kwargs))
+
+    return ultr
+
+
+def in_pattern(pattern=None, owner=False, **kwargs):
+    """Assistant's inline decorator."""
+
+    def don(func):
+        async def wrapper(event):
+            if owner and not str(event.sender_id) in owner_and_sudos():
+                res = [
+                    await event.builder.article(
                         title="CɪᴘʜᴇʀX Ⲉⲭⲥⳑυⲋⲓⳳⲉ ⲃⲟⲧ",
                         url="https://t.me/CipherXBot",
                         description="(c) CɪᴘʜᴇʀX Ⲉⲭⲥⳑυⲋⲓⳳⲉ ⲃⲟⲧ",
                         text=MSG,
                         thumb=InputWebDocument(ULTROID_PIC, 0, "image/jpeg", []),
-                        buttons=[
-                            [
-                                Button.url(
-                                    "✵CɪᴘʜᴇʀX Ⲃⲟⲧ✵",
-                                    url="https://t.me/CipherXBot",
-                                ),
-                                Button.url(
-                                    "✵Suᴩᴩᴏrᴛ Chᴀnnᴇl✵", 
-                                    url="https://t.me/FutureTechnologyOfficial"
-                                ),
-                            ]
-                        ],
+                        buttons=IN_BTTS,
                     )
-                    await event.answer(
-                        [sur],
-                        switch_pm=f"🏴‍☠: Assistant of {OWNER_NAME}",
-                        switch_pm_param="start",
-                    )
-                except BaseException:
-                    pass
+                ]
+                return await event.answer(
+                    res,
+                    switch_pm=f"🏴‍☠: Assistant of {OWNER}",
+                    switch_pm_param="start",
+                )
+            try:
+                await func(event)
+            except Exception as er:
+                LOGS.exception(er)
 
-        return wrapper
-
-    return decorator
-
-
-def asst_cmd(dec):
-    def ult(func):
-        pattern = "^/" + dec  # todo - handlers for assistant?
-        asst.add_event_handler(func, NewMessage(incoming=True, pattern=pattern))
-
-    return ult
-
-
-def callback(dat):
-    def ultr(func):
-        asst.add_event_handler(func, CallbackQuery(data=dat))
-
-    return ultr
-
-
-def inline():
-    def ultr(func):
-        asst.add_event_handler(func, InlineQuery)
-
-    return ultr
-
-
-def in_pattern(pat):
-    def don(func):
-        asst.add_event_handler(func, InlineQuery(pattern=pat))
+        asst.add_event_handler(wrapper, InlineQuery(pattern=pattern, **kwargs))
 
     return don
-
-
-# check for owner
-def owner():
-    def decorator(function):
-        @functools.wraps(function)
-        async def wrapper(event):
-            if str(event.sender_id) in owner_and_sudos():
-                await function(event)
-            else:
-                try:
-                    await event.answer(
-                        f"This is {get_display_name(ultroid.me)} ᴇxᴄlusivᴇ ʙᴏᴛ"
-                    )
-                except BaseException:
-                    pass
-
-        return wrapper
-
-    return decorator
-
-
-async def admin_check(event):
-    # Anonymous Admin Support
-    if not event.sender_id and (
-        isinstance(event.peer_id, PeerChannel)
-        and str(event.peer_id.channel_id) in str(event.chat_id)
-    ):
-        return True
-    if str(event.sender_id) in owner_and_sudos():
-        return True
-    try:
-        perms = await event.client.get_permissions(event.chat_id, event.sender_id)
-    except UserNotParticipantError:
-        return False
-    if isinstance(
-        perms.participant, (ChannelParticipantAdmin, ChannelParticipantCreator)
-    ):
-        return True
-    return False
