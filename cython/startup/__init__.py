bf698bb583d0c12f04fba4f2e2c593d904b2b7fb
CoderX <66735676+ToxygenX@users.noreply.github.com>
2021-10-28 12:31:54 +0330

    Create __init__.py

---
+++ cython/startup/__init__.py
@@ -0,0 +1,36 @@
+import os
+import time
+from logging import INFO, WARNING, FileHandler, StreamHandler, basicConfig, getLogger
+
+from safety.tools import *
+from telethon import __version__
+
+from ..version import __version__ as __pyUltroid__
+from ..version import ultroid_version
+
+if os.path.exists("cipherx.log"):
+    os.remove("cipherx.log")
+
+LOGS = getLogger("CythonLogs")
+TeleLogger = getLogger("Telethon")
+TeleLogger.setLevel(WARNING)
+
+basicConfig(
+    format="%(asctime)s || %(name)s [%(levelname)s] : %(message)s",
+    level=INFO,
+    datefmt="%m/%d/%Y, %H:%M:%S",
+    handlers=[FileHandler("cipherx.log"), StreamHandler()],
+)
+
+LOGS.info(
+    """
+                ---------------------------------------------------------
+                        Starting CɪᴘʜᴇʀX ᴇxᴄlusivᴇ ʙᴏᴛ Deployment
+                ---------------------------------------------------------
+"""
+)
+
+
+LOGS.info(f"CythonX Version - {__pyUltroid__}")
+LOGS.info(f"Telethon Version - {__version__}")
+LOGS.info(f"CɪᴘʜᴇʀX ᴇxᴄlusivᴇ ʙᴏᴛ Version - {ultroid_version}")
