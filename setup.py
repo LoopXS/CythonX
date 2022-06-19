8a0fff23b01b204869a4ad10ed7566b3301acbdf
LoopXS <91501744+LoopXS@users.noreply.github.com>
2022-06-19 13:06:20 +0430

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,9 +21,9 @@ name = "cython"
 author = "CythonX"
 description = "A Secure and Powerful Python-Telethon Based Library For CɪᴘʜᴇʀX Bot."
 license = "GNU AFFERO GENERAL PUBLIC LICENSE (v3)"
-url = "https://github.com/ToxygenX/CythonX"
+url = "https://github.com/LoopXS/CythonX"
 project_urls = {
-    "Source Code": "https://github.com/ToxygenX/CythonX",
+    "Source Code": "https://github.com/LoopXS/CythonX",
 }
 classifiers = [
     "Programming Language :: Python :: 3",
